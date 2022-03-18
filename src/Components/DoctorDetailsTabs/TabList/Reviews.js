ba23e8a56c344aa686a45ca5b4d8c6064ecbdf35
mdazim14 <mdazim.cse@gmail.com>
2022-03-18 17:38:55 +0600

    added review

--- src/Components/DoctorDetailsTabs/TabList/Reviews.js
+++ src/Components/DoctorDetailsTabs/TabList/Reviews.js
@@ -1,10 +1,98 @@
 import React from 'react';
 
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import GradeIcon from '@mui/icons-material/Grade';
+
 const Reviews = () => {
     return (
         <div>
-            <h1>Reviews</h1>
-            <h1>Reviews</h1>
+            <div>
+                <div className="d-flex p-4 border-bottom mt-4">
+
+                    <div className="col-4" >
+                        <h3>Richard Wilson</h3>
+                        <p>Reviewed 2 Days ago</p>
+                        <h6 className="text-primary ps-3" ><ThumbUpIcon /> I recommend the doctor.</h6>
+                        <div className="d-flex">
+                            <div className="text-warning">
+                                <GradeIcon />
+                                <GradeIcon />
+                                <GradeIcon />
+                                <GradeIcon />
+                            </div>
+                            <div style={{ Color: '#E0E0E0' }}>
+                                <GradeIcon />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-8 ps-5">
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quam iure maiores iste sint quibusdam dolores dicta qui commodi perspiciatis ipsum, blanditiis magnam ut tenetur sed velit nostrum! Officiis, asperiores.</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+            <div>
+                <div className="d-flex p-4 border-bottom mt-4">
+
+                    <div className="col-4" >
+                        <h3>Richard Wilson</h3>
+                        <p>Reviewed 2 Days ago</p>
+                        <h6 className="text-primary ps-3" ><ThumbUpIcon /> I recommend the doctor.</h6>
+                        <div className="d-flex">
+                            <div className="text-warning">
+                                <GradeIcon />
+                                <GradeIcon />
+                                <GradeIcon />
+                                <GradeIcon />
+                            </div>
+                            <div style={{ Color: '#E0E0E0' }}>
+                                <GradeIcon />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-8 ps-5">
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quam iure maiores iste sint quibusdam dolores dicta qui commodi perspiciatis ipsum, blanditiis magnam ut tenetur sed velit nostrum! Officiis, asperiores.</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+            <div>
+                <div className="d-flex p-4 border-bottom mt-4">
+
+                    <div className="col-4" >
+                        <h3>Richard Wilson</h3>
+                        <p>Reviewed 2 Days ago</p>
+                        <h6 className="text-primary ps-3" ><ThumbUpIcon /> I recommend the doctor.</h6>
+                        <div className="d-flex">
+                            <div className="text-warning">
+                                <GradeIcon />
+                                <GradeIcon />
+                                <GradeIcon />
+                                <GradeIcon />
+                            </div>
+                            <div style={{ Color: '#E0E0E0' }}>
+                                <GradeIcon />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-8 ps-5">
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quam iure maiores iste sint quibusdam dolores dicta qui commodi perspiciatis ipsum, blanditiis magnam ut tenetur sed velit nostrum! Officiis, asperiores.</p>
+                    </div>
+                </div>
+
+            </div>
+
+
         </div>
     );
 };
