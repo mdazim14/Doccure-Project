399790a431b7119cb3820575e946d59cccad4573
mdazim14 <mdazim.cse@gmail.com>
2022-04-30 01:09:38 +0600

    add

--- src/Components/DoctorsSideNav/Dashboard/Dashboard.jsx
+++ src/Components/DoctorsSideNav/Dashboard/Dashboard.jsx
@@ -5,6 +5,7 @@ const Dashboard = () => {
   return (
     <div>
       <h1>DashBoard</h1>
+      <h1>DashBoard</h1>
       <Row>
         <Col xs={10} md={3} className=" m-3 border border-danger">
           <h1>THis is for navbar</h1>
